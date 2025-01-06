1ffc8b203376979ad4c0e213cd3d86a4a2406b1c
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2025-01-07 04:22:47 +0530

    fix:query error , now users can easily search movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import {
-  Route,  Routes,
-} from "react-router-dom";
-import Home from "./Components/Home";
-
-
+import { Route, Routes } from "react-router-dom";
+import './App.css'
+import HomeComponents from "./Components/HomeComponents";
 // App
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/"element={<Home/>}></Route>
-     
-      </Routes>
+    <>
+    <div className=" bg-slate-800  flex  w-screen h-screen">
+        <Routes>
+          <Route path="/" element={<HomeComponents/>}></Route>
+        </Routes>
     </div>
+    </>
+  
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-
+import {Toaster} from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Toaster/>
+
     <App />
+    
+    {/* </Toaster> */}
     </BrowserRouter>
   </StrictMode>,
 )
