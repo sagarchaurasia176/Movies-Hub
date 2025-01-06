a85d1d240e5ddb792aeebc2a06157d12736da962
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2025-01-07 01:33:32 +0530

    minor changes in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import {
+  Route,  Routes,
+} from "react-router-dom";
+import Home from "./Components/Home";
 
+
+// App
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/"element={<Home/>}></Route>
+     
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    document.title = "SCSDB | Homepage"
+    return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
